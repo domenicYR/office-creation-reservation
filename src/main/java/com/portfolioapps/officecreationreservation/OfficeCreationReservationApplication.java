ef4ffa4b2323db230df140230b1d28bfaf6b622a
domenicYR <example@gmail.com>
2024-04-17 20:40:51 +0200

    Code base without any features

---
+++ src/main/java/com/portfolioapps/officecreationreservation/OfficeCreationReservationApplication.java
@@ -0,0 +1,13 @@
+package com.portfolioapps.officecreationreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeCreationReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeCreationReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolioapps/officecreationreservation/OfficeCreationReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioapps.officecreationreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeCreationReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
