d1e06262e57db9dd22988b7a341a301881d0b283
domenicYR <domenic@example.com>
2024-05-05 16:00:50 +0200

    Add statements for interacting with the database to AppController.java
    Add field and constructor for constructor injection(injection of repository bean).
    Add method call to save form input data in database.

--- src/main/java/com/portfolioapps/officecreationreservation/rest/AppController.java
+++ src/main/java/com/portfolioapps/officecreationreservation/rest/AppController.java
@@ -1,12 +1,24 @@
 package com.portfolioapps.officecreationreservation.rest;
 
 import com.portfolioapps.officecreationreservation.OfficeCreationForm;
+import com.portfolioapps.officecreationreservation.OfficeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class AppController {
+    // Field(s)
+    private OfficeRepository officeRepository;
+
+    // Constructor(s)
+    @Autowired
+    public AppController(OfficeRepository repository) {
+        this.officeRepository = repository;
+    }
+
+    // Method(s)
     @GetMapping("/")
     public String displayStartPage() {
         return "index";
@@ -25,6 +37,7 @@ public class AppController {
 
     @PostMapping("/officeCreation")
     public String createOfficeSubmit(@ModelAttribute OfficeCreationForm formData, Model model) {
+        officeRepository.save(formData);
         model.addAttribute("office", formData);
         return "result";
     }
