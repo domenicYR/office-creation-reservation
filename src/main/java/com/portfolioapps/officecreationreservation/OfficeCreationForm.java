22a4b6336f621a4ad40599821b9ba954c88d24ee
domenicYR <domenic@example.com>
2024-05-05 15:23:52 +0200

    Add field "id", getter and setter method for "id" and JPA annotations to OfficeCreationForm.java

--- src/main/java/com/portfolioapps/officecreationreservation/OfficeCreationForm.java
+++ src/main/java/com/portfolioapps/officecreationreservation/OfficeCreationForm.java
@@ -1,11 +1,30 @@
 package com.portfolioapps.officecreationreservation;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "offices")
 public class OfficeCreationForm {
     // Field(s)
+    // "id" field corresponds primary key
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "officeName")
     private String officeName;
+    @Column(name = "officeOpeningTimes")
     private String officeOpeningTimes;
 
     // Method(s)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
     public String getOfficeName() {
         return officeName;
     }
