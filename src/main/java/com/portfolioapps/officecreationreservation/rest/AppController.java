7c34cfee001dca66061eea67bf24c0ce2bef2433
domenicYR <domenic@example.com>
2024-05-22 09:07:07 +0200

    Create static variable for current office object, field for repository bean and handle requests for deleting an office and adding a room
    Create static variable to save a requested office object from the database and to use it in the method displayRooms and handleAddRoomFormData.
    
    Create field for repository bean of type RoomRepository to interact with the database.

--- src/main/java/com/portfolioapps/officecreationreservation/rest/AppController.java
+++ src/main/java/com/portfolioapps/officecreationreservation/rest/AppController.java
@@ -1,7 +1,9 @@
 package com.portfolioapps.officecreationreservation.rest;
 
+import com.portfolioapps.officecreationreservation.AddRoomForm;
 import com.portfolioapps.officecreationreservation.OfficeCreationForm;
 import com.portfolioapps.officecreationreservation.OfficeRepository;
+import com.portfolioapps.officecreationreservation.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,13 +11,17 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class AppController {
+    private static OfficeCreationForm currentOffice;
+
     // Field(s)
     private OfficeRepository officeRepository;
+    private RoomRepository roomRepository;
 
     // Constructor(s)
     @Autowired
-    public AppController(OfficeRepository repository) {
-        this.officeRepository = repository;
+    public AppController(OfficeRepository officeRepository, RoomRepository roomRepository) {
+        this.officeRepository = officeRepository;
+        this.roomRepository = roomRepository;
     }
 
     // Method(s)
@@ -43,14 +49,36 @@ public class AppController {
         return "result";
     }
 
-    // handle request for showing rooms
+    // handle request for showing rooms to an office
     @GetMapping("/show-rooms/{id}")
     public String displayRooms(@PathVariable("id") Integer id, Model model) {
-        OfficeCreationForm officeWithRequestedID = officeRepository.findById(id)
+        currentOffice = officeRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid Id: " + id));
 
-        model.addAttribute("officeWithRequestedID", officeWithRequestedID);
+        model.addAttribute("currentOffice", currentOffice);
 
         return "show-rooms";
+    }
+
+    // handle request for deleting an office
+    @GetMapping("/delete-rooms/{id}")
+    public String deleteOffice(@PathVariable("id") Integer id) {
+        this.officeRepository.deleteById(id);
+        return "redirect:/offices";
+    }
+
+    // handle request for showing form to add a room
+    @GetMapping("/show-rooms/add-room")
+    public String displayAddRoomForm(Model model) {
+        model.addAttribute("room", new AddRoomForm());
+        return "add-room";
+    }
+
+    @PostMapping("/show-rooms/add-room")
+    public String handleAddRoomFormData(@ModelAttribute AddRoomForm roomFormData, Model model) {
+        roomFormData.setOffice(currentOffice);
+        this.roomRepository.save(roomFormData);
+        model.addAttribute("room", roomFormData);
+        return "resultRoom";
     }
 }
