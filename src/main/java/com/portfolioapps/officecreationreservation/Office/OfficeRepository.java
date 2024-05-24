11d4781a915a1214d4972ce340aafad7ea96e290
domenicYR <domenic@example.com>
2024-05-24 18:48:16 +0200

    Create query to get all offices in ascending order

--- src/main/java/com/portfolioapps/officecreationreservation/Office/OfficeRepository.java
+++ src/main/java/com/portfolioapps/officecreationreservation/Office/OfficeRepository.java
@@ -1,10 +1,14 @@
 package com.portfolioapps.officecreationreservation.Office;
 
 import com.portfolioapps.officecreationreservation.Office.Office;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OfficeRepository extends CrudRepository<Office, Integer> {
-
+    @Query(value = "select * from offices o order by o.office_name asc", nativeQuery = true)
+    List<Office> findAllOfficesOrderASC();
 }
