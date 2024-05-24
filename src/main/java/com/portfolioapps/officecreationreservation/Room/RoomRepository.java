b88119141002157f7d2a3cb7fdbfb329a841ab3a
domenicYR <domenic@example.com>
2024-05-24 08:38:08 +0200

    Create query to get all rooms associated with a specific office

--- src/main/java/com/portfolioapps/officecreationreservation/Room/RoomRepository.java
+++ src/main/java/com/portfolioapps/officecreationreservation/Room/RoomRepository.java
@@ -1,10 +1,13 @@
 package com.portfolioapps.officecreationreservation.Room;
 
-import com.portfolioapps.officecreationreservation.Room.Room;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RoomRepository extends CrudRepository<Room, Integer> {
-
+    @Query(value = "select * from rooms r where r.office_id=?1 order by r.room_name asc", nativeQuery = true)
+    List<Room> findAllRoomsByForeignKey(Integer id);
 }
