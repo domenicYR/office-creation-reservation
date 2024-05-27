5dfe33cb8117456449ba7044f960f5e282a88796
domenicYR <domenic@example.com>
2024-05-27 07:44:31 +0200

    Add file Reservation.java and class as entity

---
+++ src/main/java/com/portfolioapps/officecreationreservation/Reservation/Reservation.java
@@ -0,0 +1,64 @@
+package com.portfolioapps.officecreationreservation.Reservation;
+
+import com.portfolioapps.officecreationreservation.Room.Room;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "reservations")
+public class Reservation {
+    // Field(s)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "reservationDate")
+    private String reservationDate;
+    @Column(name = "reservationTimeFrom")
+    private String reservationTimeFrom;
+    @Column(name = "reservationTimeTo")
+    private String reservationTimeTo;
+
+    @ManyToOne
+    private Room room;
+
+    // Method(s)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getReservationDate() {
+        return reservationDate;
+    }
+
+    public void setReservationDate(String reservationDate) {
+        this.reservationDate = reservationDate;
+    }
+
+    public String getReservationTimeFrom() {
+        return reservationTimeFrom;
+    }
+
+    public void setReservationTimeFrom(String reservationTimeFrom) {
+        this.reservationTimeFrom = reservationTimeFrom;
+    }
+
+    public String getReservationTimeTo() {
+        return reservationTimeTo;
+    }
+
+    public void setReservationTimeTo(String reservationTimeTo) {
+        this.reservationTimeTo = reservationTimeTo;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+}
