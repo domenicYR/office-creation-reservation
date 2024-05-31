75f6c8eb1f1a708fa3f860f87d90839664a870e7
domenicYR <domenic@example.com>
2024-05-31 14:25:38 +0200

    Create query to get all reservations for a specific room and date

--- src/main/java/com/portfolioapps/officecreationreservation/Reservation/ReservationRepository.java
+++ src/main/java/com/portfolioapps/officecreationreservation/Reservation/ReservationRepository.java
@@ -1,9 +1,16 @@
 package com.portfolioapps.officecreationreservation.Reservation;
 
+import com.portfolioapps.officecreationreservation.Room.Room;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReservationRepository extends CrudRepository<Reservation, Integer> {
-
+    @Query(value = "select * from reservations r " +
+            "where r.reservation_date=?1 and r.room_id=?2 " +
+            "order by r.reservation_time_from asc", nativeQuery = true)
+    List<Reservation> findAllReservationsByDateAndRoomID(String date, Integer roomID);
 }
